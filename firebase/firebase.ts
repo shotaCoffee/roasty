87d1a09e627c29bf07c9abc0f3250bdaa7bde509
shotaCoffee <dripcoffee1018@gmail.com>
2020-07-19 00:42:51 +0900

    remove chache

--- firebase/firebase.ts
+++
@@ -1,8 +0,0 @@
-import firebase from 'firebase';
-import 'firebase/firestore';
-import firebaseConfig from './firebaseConfig';
-
-firebase.initializeApp(firebaseConfig);
-
-export const auth = firebase.auth();
-export default firebase;
